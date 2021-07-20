305f1b5c273b1db63900928ee3070ae018aac751
mohammed osama <mohammedosama@ieee.org>
2021-07-20 05:23:12 +0200

    initial commit

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,29 @@
+<script lang="tsx">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseInput',
+  props: {
+    label: {
+      required: true,
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+  setup(props, { attrs, emit }) {
+    const handleInputChange = (e: Event) => {
+      emit('update:modelValue', e.target.value);
+    };
+    return () => (
+      <>
+        <label>{props.label}</label>
+        <input value={props.modelValue} onInput={handleInputChange} {...attrs} placeholder={props.label} class="field" />
+      </>
+    );
+  },
+});
+</script>
---
+++ src/views/SimpleForm.vue
@@ -0,0 +1,99 @@
+<script lang="tsx">
+import { defineComponent, reactive } from 'vue';
+import BaseInput from '@/components/BaseInput.vue';
+
+export default defineComponent({
+  components: {
+    BaseInput,
+  },
+  setup() {
+    const state = reactive({
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community',
+      ],
+      event: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false,
+        },
+      },
+
+    });
+    return () => (
+      <div>
+        <h1>Create an event</h1>
+        <form>
+          <label>Select a category</label>
+          <select v-model={state.event.category}>
+            {state.categories.map((category) => (
+              <option
+                value={category}
+                key={category}
+                selected={category === state.event.category}
+              >
+                {category}
+              </option>
+            ))}
+          </select>
+
+          <h3>Name & describe your event</h3>
+          <BaseInput
+            v-model={state.event.title}
+            label="Title"
+            type="text"
+          />
+          <BaseInput
+            v-model={state.event.description}
+            label="Description"
+            type="text"
+
+          />
+
+          <h3>Where is your event?</h3>
+
+          <BaseInput
+            v-model={state.event.location}
+            label="Location"
+            type="text"
+          />
+
+          <h3>Are pets allowed?</h3>
+          <div>
+            <input type="radio" v-model={state.event.pets} value="1" name="pets" />
+            <label>Yes</label>
+          </div>
+
+          <div>
+            <input type="radio" v-model={state.event.pets} value="0" name="pets" />
+            <label>No</label>
+          </div>
+
+          <h3>Extras</h3>
+          <div>
+            <input type="checkbox" v-model={state.event.extras.catering} class="field" />
+            <label>Catering</label>
+          </div>
+
+          <div>
+            <input type="checkbox" v-model={state.event.extras.music} class="field" />
+            <label>Live music</label>
+          </div>
+
+          <button class="button -fill-gradient" type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  },
+});
+</script>
